d506102236f4c22291e257a1c128d751ad42fd81
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-08-23 15:40:27 +0000

    Protect Routes with Guards

--- iluvcoffee/src/main.ts
+++ iluvcoffee/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 import { HttpExceptionFilter } from './common/filter/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
     }
   }));
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(3000);
 }
 bootstrap();
