5172651d61eba9557fd2993bc7b3bd7ee5695c22
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-08-29 16:42:35 +0000

    decorating model properties

--- iluvcoffee/src/coffees/dto/create-coffee.dto.ts
+++ iluvcoffee/src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,22 @@
-import { IsString, IsDefined } from 'class-validator';
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCoffeeDto {
+  @ApiProperty({
+    description: 'the name of the coffee.'
+  })
   @IsString()
   readonly name: string;
   
+  @ApiProperty({
+    description: 'the brand of the coffee.'
+  })
   @IsString()
   readonly brand: string;
   
+  @ApiProperty({
+    example: []
+  })
   @IsString({ each: true })
   readonly flavors: string[];
 }
