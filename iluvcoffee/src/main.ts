e1d103b331438ac2e1cc6885500e5c1816319aed
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-08-29 16:30:40 +0000

    docs setup

--- iluvcoffee/src/main.ts
+++ iluvcoffee/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+import { AppModule } from './app.module';
 
 import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
-// import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,17 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor()
   );
+
+  // Setting up Swagger document 
+  const options = new DocumentBuilder()
+  .setTitle('Iluvcoffee')
+  .setDescription('Coffee application')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(3000);
 }
