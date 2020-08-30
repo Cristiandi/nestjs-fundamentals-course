c78976c3e26a08abf79fe57d078575b6bc25e026
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-08-30 17:29:49 +0000

    creating a model with mongoose

--- iluvcoffee/src/app.module.ts
+++ iluvcoffee/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -6,8 +7,11 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
